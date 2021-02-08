b63b5aab7f4e22737a42f5ad24c5f7782ceaa5a7
Vincent Zhang <vincent@unity3d.com>
2021-02-08 16:00:35 +0800

    Be able to draw unity to a well-layouted SurfaceView.

--- app/src/main/java/com/self/viewtoglrendering/MainActivity.java
+++ app/src/main/java/com/self/viewtoglrendering/MainActivity.java
@@ -1,23 +1,15 @@
 package com.self.viewtoglrendering;
 
-import android.content.Intent;
 import android.content.res.Configuration;
-import android.graphics.PixelFormat;
-import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.view.ViewGroup;
-import android.webkit.WebChromeClient;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import android.widget.FrameLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.unity3d.player.R;
-import com.self.viewtoglrendering.cuberenerer.CubeGLRenderer;
 import com.unity3d.player.UnityPlayer;
 
 public class MainActivity extends AppCompatActivity
@@ -32,12 +24,20 @@ public class MainActivity extends AppCompatActivity
     }
 
     private void initViews() {
+        // Create a SurfaceView and bring it to the front.
         mSurfaceView = new SurfaceView(this);
         mSurfaceView.setZOrderOnTop(true);
+
+        // Set the SurfaceView layout parameters.
+        FrameLayout.LayoutParams unityPlayerLayoutParams = new FrameLayout.LayoutParams(600, 800, Gravity.TOP);
+        unityPlayerLayoutParams.setMargins(100, 100, 0, 0);
+        mSurfaceView.setLayoutParams(unityPlayerLayoutParams);
+
+        // Add callbacks for Surface events.
         mSurfaceView.getHolder().addCallback(new SurfaceHolder.Callback(){
             public final void surfaceCreated(SurfaceHolder surfaceHolder) {
-                Log.v("Unity", "Change Surface");
-                surfaceHolder.setFixedSize(150, 200);
+                // This is where we set the Surface to UnityPlayer to draw.
+                surfaceHolder.setFixedSize(600, 800);
                 mUnityPlayer.displayChanged(0, surfaceHolder.getSurface());
             }
 
@@ -49,33 +49,34 @@ public class MainActivity extends AppCompatActivity
                 mUnityPlayer.displayChanged(0, null);
             }
         });
+
+        // Set the content view to the created SurfaceView.
         setContentView(mSurfaceView);
 
+        // Create UnityPlayer
         mUnityPlayer = new UnityPlayer(this);
-//        addContentView(mUnityPlayer, new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-
         mUnityPlayer.requestFocus();
     }
 
-//    protected void onDestroy() {
-//        this.mUnityPlayer.destroy();
-//        super.onDestroy();
-//    }
-//
-//    protected void onPause() {
-//        super.onPause();
-//        this.mUnityPlayer.pause();
-//    }
+    protected void onDestroy() {
+        this.mUnityPlayer.destroy();
+        super.onDestroy();
+    }
+
+    protected void onPause() {
+        super.onPause();
+        this.mUnityPlayer.pause();
+    }
 
     protected void onResume() {
         super.onResume();
         this.mUnityPlayer.resume();
     }
 
-//    public void onConfigurationChanged(Configuration newConfig) {
-//        super.onConfigurationChanged(newConfig);
-//        this.mUnityPlayer.configurationChanged(newConfig);
-//    }
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        this.mUnityPlayer.configurationChanged(newConfig);
+    }
 
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
